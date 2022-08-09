4fd654cc97c5475ddbff7332c4d7fddb8d604f2e
Karl Chvojka <karl.chvojka@protonmail.com>
2022-08-09 14:55:20 -0400

    IMP: Header Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+// Framework Imports
 import React from 'react'
+
+// Library Imports
+import Header from './components/modules/Header/Header'
+
+// CSS Imports
+import StyledApp from './StyledApp'
 
 const App = () => {
   return (
-    <h1>Welcome to React App thats build using Webpack and Babel separately</h1>
+    <StyledApp>
+      <Header />
+      <h1>Welcome to React App thats build using Webpack and Babel separately</h1>
+    </StyledApp>
   )
 }
 
---
+++ src/components/modules/Header/StyledHeader.js
@@ -0,0 +1,9 @@
+// Library Imports
+import styled from 'styled-components'
+
+const StyledHeader = styled.section`
+  border: 1px solid #000;
+  width: 100%;
+`
+
+export default StyledHeader
