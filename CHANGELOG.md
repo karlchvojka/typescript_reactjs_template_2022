635852b094844f500a765b0cbae83352449e7a4f
Karl Chvojka <karl.chvojka@protonmail.com>
2022-08-29 14:33:31 -0400

    FIX: Webpack configuration build issue.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -28,7 +28,11 @@ Initial building of the Template.
 ## [1.0.3] - 2022-08-29
 Issue fixing
 
+Note: #3 was done first to establish folder/file settings for the mobile setup
+
 [#3 Global Config](https://github.com/karlchvojka/reactjs_template_2022/issues/3)\
+[#2 Default Mobile Setup](https://github.com/karlchvojka/reactjs_template_2022/issues/2)\
+[#6 Webpack configuration](https://github.com/karlchvojka/reactjs_template_2022/issues/6)
 
 ### Added
 
@@ -46,6 +50,17 @@ Issue fixing
 - Added /src/globalConfig/templates folder
 - Added /src/globalConfig/templates/styles.js file
 
+#### #6
+- Added /public/favicon.ico
+- Added /public/index.html
+- Added /public/manifest.json
+- Added /public/robots.txt
+- Added /public/sitemap.xml
+- Added /src/template.html
+- Installed babel-loader (8.2.5)
+- Installed html-loader (4.1.0)
+- Installed html-webpack-plugin (5.5.0)
+
 ### Changed
 
 #### #3
@@ -59,11 +74,31 @@ Issue fixing
   - Added no-static-element-interactions rule
 - /src/globalConfig/GlobalStyles.js
   - Added Mobile import
-  - Added default mobile breakpoints.
+  - Added default mobile breakpoints
 - /src/StyledApp.js
   - Added Mobile import
-  - Added default mobile breakpoints.
+  - Added default mobile breakpoints
 - /src/components/modules/Header/StyledHeader.js
   - Added Mobile import
-  - Added default mobile breakpoints.
+  - Added default mobile breakpoints
+
+#### #6
+- webpack.config.js
+  - Added comments throughout the code.
+  - Integrated path library usage within:
+    - entry
+    - output
+    - devServer
+  - Added rules for file types:
+    - js/jsx
+    - png/jpg/jpeg/gif
+    - font files
+    - html
+  - Added plugin settings
+    - HtmlWebPackPlugin settings
+
+### Removed
+
+#### #6
+- Removed /cypress folder
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,48 @@
+// Framework Imports
 const path = require('path')
+
+// Plugin Library Imports
+const HtmlWebPackPlugin = require("html-webpack-plugin")
 
 module.exports = {
   mode: 'development',
-  entry: './index.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'main.js',
+  /** 
+   * Sets the entry point for the app post build.
+   * Configured entry for the index.js file.
+   */
+  entry: {
+    main: path.resolve(__dirname, './index.js'),
   },
+
+  /**
+   * Sets where the files that are built are saved.
+   */
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+  },
+
   target: 'web',
+
+  /**
+   * Dev server configuration
+   */
   devServer: {
-    port: '9500',
-    static: ['./public'],
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
     open: true,
     hot: true,
     liveReload: true,
+    historyApiFallback: true,
+    port: process.env.PORT || 3000,
+    allowedHosts: 'all'
   },
   resolve: {
+
+    /**
+     * Configuration of aliases.
+     */
     alias: {
       path: require.resolve('path-browserify'),
       public: path.resolve(__dirname, './public'),
@@ -27,15 +54,47 @@ module.exports = {
       pages: path.resolve(__dirname, './src/components/pages'),
       templates: path.resolve(__dirname, './src/components/templates'),
     },
+
     extensions: ['.js', '.jsx', '.json'],
   },
   module: {
+
+    /**
+     * Rules configuration for file type imports.
+     */
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: 'babel-loader',
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
     ],
   },
+
+  /**
+   * Plugin configurations.
+   */
+   plugins: [
+    
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, './src/template.html'), // template file
+      filename: 'index.html', // output file
+    })
+  ]
 }
